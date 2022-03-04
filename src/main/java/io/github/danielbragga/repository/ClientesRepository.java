65e03f0d1b74832291c51626cc1af94eb07ccf85
Daniel Braga <danielbraga2121@gmail.com>
2022-03-03 23:10:11 -0300

    first commit

---
+++ src/main/java/io/github/danielbragga/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package io.github.danielbragga.repository;
+
+
+import io.github.danielbragga.nodel.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+    }
+}
