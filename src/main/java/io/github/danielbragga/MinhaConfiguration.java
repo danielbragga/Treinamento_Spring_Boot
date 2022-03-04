0617b3969abbe81b267047f194e097393ae3e89f
danielbragga <danielbraga2121@gmail.com>
2022-03-04 14:21:09 -0300

    finish section 2

--- src/main/java/io/github/danielbragga/MinhaConfiguration.java
+++ src/main/java/io/github/danielbragga/MinhaConfiguration.java
@@ -1,12 +1,18 @@
 package io.github.danielbragga;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 
-@Configuration
+
+@Development
 public class MinhaConfiguration {
-    @Bean (name = "applicationName")
-    public String applicationName () {
-        return "Sistema de Vendas";
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+
+
+
+        };
     }
 }
--- src/main/java/io/github/danielbragga/VendasApplication.java
+++ src/main/java/io/github/danielbragga/VendasApplication.java
@@ -2,8 +2,11 @@ package io.github.danielbragga;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VendasApplication {
-
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
+
+    @Cachorro
+    private Animal animal;
+
+
+    @Bean(name = "executarAnimal")
+    public CommandLineRunner executar() {
+        return args -> {
+            this.animal.fazerBarulho();
+        };
+    }
 
 
     @GetMapping("/hello")
     public String helloWord(){
-        return "applicationName";
+        return "Sistema de Vendas";
 
     }
 
